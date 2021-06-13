6fa473fad88c5558d628ba98a51e1511f83ea7f9
begumozb <66123462+begumozb@users.noreply.github.com>
2021-06-13 15:38:11 +0300

    Update usedJquery.js

--- js/usedJquery.js
+++ js/usedJquery.js
@@ -103,17 +103,3 @@ $(function(){
   $("#birthday").datepicker();
 });
 
-/*var btn = document.getElementById("btn");
-
-btn.addEventListener("Click", function(){
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'adoption.json');
-  ourRewqest.onload = function(){
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-  
-  }
-  ourRewqest.send();
-});*/
-
-let animals = [];
